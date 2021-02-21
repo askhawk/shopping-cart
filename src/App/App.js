f94aca631543217b12ee8848f6494dea04de4919
askhawk <asknet@gmail.com>
2021-02-21 03:59:17 -0500

    products files

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,44 @@
-// feature 1
-// Added another commect
 import React from 'react';
-
+import data from '../data.json';
+import Products from '../components/products/Products';
 import './app.css';
-function App() {
+//
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+}
+
+  render() {
+    return (
+      <div className='grid-container'>
+      <header className='App-header'>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.props.products}></Products>
+            </div>
+            <div className="sidebar">
+              Cart Items
+            </div>
+        </div>
+      </main>
+      <footer>
+        Copyright © 2021 Coding With Asknet.
+      </footer>
+    </div>
+    )
+  }
+}
+
+/* function App() {
   return (
     <div className='grid-container'>
       <header className='App-header'>
@@ -17,6 +52,6 @@ function App() {
       </footer>
     </div>
   );
-}
+} */
 
 export default App;
