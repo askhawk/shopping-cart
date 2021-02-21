254ba2f9ef2bbb16958b90a5ede1763f4defe60a
askhawk <asknet@gmail.com>
2021-02-20 21:22:09 -0500

    Signed-off-by: askhawk <asknet@gmail.com>

---
+++ src/App/App.js
@@ -0,0 +1,22 @@
+// feature 1
+// Added another commect
+import React from 'react';
+
+import './app.css';
+function App() {
+  return (
+    <div className='grid-container'>
+      <header className='App-header'>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        Copyright © 2021 Coding With Asknet.
+      </footer>
+    </div>
+  );
+}
+
+export default App;
