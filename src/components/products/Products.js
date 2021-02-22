9913e63e0c6fe2be7823f70e0ee490de7a6f328c
askhawk <asknet@gmail.com>
2021-02-21 20:53:41 -0500

    Filter section was done

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import formatCurrency from '../../utilities/utils';
-import './Products.css';
+import React, { Component } from "react";
+import formatCurrency from "../../utilities/utils";
+import "./Products.css";
 
 class Products extends Component {
-
-  render(props) {
-    console.log(this.props.products)
-    return (
-      
-      <div>
-        <ul className="products">
-          {this.props.products.map((product) => (
-            <li key={product._id}>
-              <div className="product">
-                <a href={"#" + product._id}>
-                  <img src={product.image} alt={product.title} />
-                  <p>{product.title}</p>
-                </a>
-                <div className="product-price">
-                  <div>{formatCurrency(product.price)}</div>
-                  <button className="button primary">
-                    Add To Cart
-                  </button>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+	render(props) {
+		//console.log(this.props.products)
+		return (
+			<div>
+				<ul className="products">
+					{this.props.products.map((product) => (
+						<li key={product._id}>
+							<div className="product">
+								<a href={"#" + product._id}>
+									<img src={product.image} alt={product.title} />
+									<p>{product.title}</p>
+								</a>
+								<div className="product-price">
+									<div>{formatCurrency(product.price)}</div>
+									<button className="button primary">Add To Cart</button>
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
+	}
 }
 export default Products;
